bf51df109d04ea4a11ad089c01fefe13dd59bc47
Jeff Pak <jeffrey.pak@emc.com>
2016-04-22 15:13:09 -0700

    update recipes and install go tools individually
    - Add go, remove luan_vim, update homebrew
    
    [#117001189]
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- cli/recipes/default.rb
+++ cli/recipes/default.rb
@@ -1,4 +1,3 @@
 include_recipe 'cli::concourse_cli'
 include_recipe 'cli::install_checkman'
-include_recipe 'cli::luan_vim'
 include_recipe 'cli::install_go_tools'
--- cli/recipes/install_go_tools.rb
+++ cli/recipes/install_go_tools.rb
@@ -1,11 +1,35 @@
-execute 'install go utils' do
+execute 'make go dir' do
   command 'mkdir -p ~/go'
+end
+
+execute 'install ginkgo' do
   command 'GOPATH=$HOME/go go get -u github.com/onsi/ginkgo/ginkgo'
+end
+
+execute 'install ginkgo' do
   command 'GOPATH=$HOME/go go get -u github.com/onsi/gomega'
+end
+
+execute 'install counterfeiter' do
   command 'GOPATH=$HOME/go go get -u github.com/maxbrunsfeld/counterfeiter'
+end
+
+execute 'install gvt' do
   command 'GOPATH=$HOME/go go get -u github.com/FiloSottile/gvt'
+end
+
+execute 'install godep' do
   command 'GOPATH=$HOME/go go get -u github.com/tools/godep'
+end
+
+execute 'install go-bindata' do
   command 'GOPATH=$HOME/go go get -u github.com/jteeuwen/go-bindata/...'
+end
+
+execute 'install goi18n' do
   command 'GOPATH=$HOME/go go get -u github.com/nicksnyder/go-i18n/goi18n'
+end
+
+execute 'install i18n4go' do
   command 'GOPATH=$HOME/go go get -u github.com/krishicks/i18n4go/i18n4go'
 end
